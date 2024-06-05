a450b70d5be3c867412b847a2ac59522aca36cff
dasv21 <david@santiago.com>
2024-06-05 09:30:15 -0400

    Refactorice el Css de editBuyBillForm

--- src/components/EditBuyBillForm.jsx
+++ src/components/EditBuyBillForm.jsx
@@ -51,160 +51,145 @@ function EditBuyBillForm() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="max-w-2xl mx-auto p-4 bg-white shadow-md rounded-lg"
-    >
-      <h1 className="text-2xl text-center font-bold mb-4">
-        Editar Factura de Compra
-      </h1>
-      <div className="grid grid-cols-2">
-        <div className="mx-2">
-          <div className="mb-2">
-            <label className="block text-gray-700">Numero de Factura</label>
-            <input
-              type="text"
-              name="numBill"
-              value={bill.numBill}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Número de Control</label>
-            <input
-              type="text"
-              name="numControl"
-              value={bill.numControl}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Proveedor</label>
-            <input
-              type="text"
-              name="nameProvider"
-              value={bill.nameProvider}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">RIF del Proveedor</label>
-            <input
-              type="text"
-              name="rifProvider"
-              value={bill.rifProvider}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Fecha de Creación</label>
-            <input
-              type="date"
-              name="dateCreatedOn"
-              value={bill.dateCreatedOn}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Fecha de Aplicacion</label>
-            <input
-              type="date"
-              name="dateApplicationOn"
-              value={bill.dateApplicationOn}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2 col-span-2">
-            <label className="block text-gray-700">Observacion</label>
-            <input
-              type="text"
-              name="observation"
-              value={bill.observation}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
+    <form className="form-container" onSubmit={handleSubmit}>
+      <h1 className="form-heading">Editar Factura de Compra</h1>
+      <div className="form-grid">
+        <div>
+          <label className="form-label">Numero de Factura</label>
+          <input
+            type="text"
+            name="numBill"
+            value={bill.numBill}
+            onChange={handleInputChange}
+            className="form-input"
+          />
         </div>
         <div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Tipo de Transacción</label>
-            <input
-              type="text"
-              name="transactionType"
-              value={bill.transactionType}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Monto Exento</label>
-            <input
-              type="text"
-              name="exemptAmount"
-              value={bill.exemptAmount}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Monto IVA</label>
-            <input
-              type="text"
-              name="ivaAmount"
-              value={bill.ivaAmount}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Total de Compra</label>
-            <input
-              type="text"
-              name="totalBuy"
-              value={bill.totalBuy}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Impuesto Especial</label>
-            <input
-              type="text"
-              name="taxEspecial"
-              value={bill.taxEspecial}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700">Tipo de Compra</label>
-            <input
-              type="text"
-              name="buyType"
-              value={bill.buyType}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
+          <label className="form-label">Número de Control</label>
+          <input
+            type="text"
+            name="numControl"
+            value={bill.numControl}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Proveedor</label>
+          <input
+            type="text"
+            name="nameProvider"
+            value={bill.nameProvider}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">RIF del Proveedor</label>
+          <input
+            type="text"
+            name="rifProvider"
+            value={bill.rifProvider}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Fecha de Creación</label>
+          <input
+            type="date"
+            name="dateCreatedOn"
+            value={bill.dateCreatedOn}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Fecha de Aplicacion</label>
+          <input
+            type="date"
+            name="dateApplicationOn"
+            value={bill.dateApplicationOn}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Observacion</label>
+          <input
+            type="text"
+            name="observation"
+            value={bill.observation}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Tipo de Transacción</label>
+          <input
+            type="text"
+            name="transactionType"
+            value={bill.transactionType}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Monto Exento</label>
+          <input
+            type="text"
+            name="exemptAmount"
+            value={bill.exemptAmount}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Monto IVA</label>
+          <input
+            type="text"
+            name="ivaAmount"
+            value={bill.ivaAmount}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Total de Compra</label>
+          <input
+            type="text"
+            name="totalBuy"
+            value={bill.totalBuy}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Impuesto Especial</label>
+          <input
+            type="text"
+            name="taxEspecial"
+            value={bill.taxEspecial}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label className="form-label">Tipo de Compra</label>
+          <input
+            type="text"
+            name="buyType"
+            value={bill.buyType}
+            onChange={handleInputChange}
+            className="form-input"
+          />
         </div>
       </div>
-      <div className="flex justify-between mt-2">
-        <Link
-          to={`/clients/${id}/buy-bill`}
-          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 "
-        >
+      <div className="box-button">
+        <Link to={`/clients/${id}/buy-bill`} className="button">
           Volver
         </Link>
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
-        >
+        <button type="submit" className="button">
           Guardar Cambios
         </button>
       </div>
